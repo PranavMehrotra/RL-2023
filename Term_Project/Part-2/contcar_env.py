c030e18524d7d29db56982ec185b6f3568a85d2d
PranavMehrotra <87844730+PranavMehrotra@users.noreply.github.com>
2023-11-06 01:46:06 +0530

    Environment updated, made more efficient 1. Improve efficiency of radar ray casting using numpy arrays. 2. Using observation numpy array. 3. Add half lap check and reward. 4. Correct all the checks and fix some errors.

--- Term_Project/Part-2/contcar_env.py
+++ Term_Project/Part-2/contcar_env.py
@@ -3,7 +3,7 @@ import math
 import numpy as np
 
 class ContinuousCarRadarEnv:
-    def __init__(self, window_size=(800, 900)):
+    def __init__(self, window_size=(700, 900), radius=300, path_thickness=100, car_length=30, car_width=20, car_speed=1., num_rays=5, ray_lengths=[60, 60, 60, 60, 60], ray_angles=[math.pi / 4, math.pi / 8, 0, -math.pi / 8, -math.pi / 4], reward_inside_path=1, reward_outside_path=-1):
         self.window_size = window_size
 
         # Pygame initialization
@@ -18,27 +18,32 @@ class ContinuousCarRadarEnv:
 
         # Circular path parameters
         self.center = (self.window_size[0] // 2, self.window_size[1] // 2)
-        self.radius = 200
-        self.path_thickness = 50
+        self.radius = radius
+        self.path_thickness = path_thickness
+        self.inner_radius = self.radius - self.path_thickness
 
         # Car parameters
-        self.car_length = 30
-        self.car_width = 20
-        self.car_speed = 1.
+        self.car_length, self.car_width, self.car_speed = car_length, car_width, car_speed
         self.car_angle = math.pi / 2  # Start facing upward
-        self.car_x, self.car_y = self.center[0] + self.radius - 25, self.center[1]
+        self.car_x, self.car_y = self.center[0] + self.radius - self.path_thickness/2, self.center[1]
 
         # Radar parameters
-        self.num_rays = 5
-        self.ray_lengths = [50, 75, 100, 125, 150]
-        self.ray_angles = [-math.pi / 4, -math.pi / 8, 0, math.pi / 8, math.pi / 4]
-        self.ray_values = [0] * self.num_rays  # Store radar ray values (1, -1, or 0)
+        # Check if ray_lengths and ray_angles are of the same length and equal to num_rays
+        if len(ray_lengths) != len(ray_angles) or len(ray_lengths) != num_rays:
+            raise Exception("ray_lengths and ray_angles must have length = num_rays")
+        self.num_rays, self.ray_lengths, self.ray_angles = num_rays, np.array(ray_lengths), np.array(ray_angles)
+        # self.ray_values = np.zeros(self.num_rays) # Initialize radar values to 0
 
         # Rewards and score
-        self.reward_inside_path = 1
-        self.reward_outside_path = -1
+        self.reward_inside_path = reward_inside_path
+        self.reward_outside_path = reward_outside_path
         self.score = 0
+        self.half_lap_done = False
 
+        # Observation space
+        self.observation = np.zeros(self.num_rays + 3) # 3 extra values for car_x, car_y, car_angle
+        # Update observation
+        self.observation[-3:] = [self.car_x, self.car_y, self.car_angle]
         # Initialize Pygame clock
         self.clock = pygame.time.Clock()
 
@@ -63,15 +68,27 @@ class ContinuousCarRadarEnv:
         self.car_y %= self.window_size[1]
 
         # Cast radar rays and update ray_values
-        for i in range(self.num_rays):
-            ray_x = self.car_x + self.ray_lengths[i] * math.cos(self.car_angle + self.ray_angles[i])
-            ray_y = self.car_y - self.ray_lengths[i] * math.sin(self.car_angle + self.ray_angles[i])
+        # Inefficient way
+        # for i in range(self.num_rays):
+        #     ray_x = self.car_x + self.ray_lengths[i] * math.cos(self.car_angle + self.ray_angles[i])
+        #     ray_y = self.car_y - self.ray_lengths[i] * math.sin(self.car_angle + self.ray_angles[i])
 
-            distance_to_center = math.sqrt((ray_x - self.center[0]) ** 2 + (ray_y - self.center[1]) ** 2)
-            if distance_to_center < self.radius:
-                self.ray_values[i] = 1
-            else:
-                self.ray_values[i] = -1
+        #     distance_to_center = math.sqrt((ray_x - self.center[0]) ** 2 + (ray_y - self.center[1]) ** 2)
+        #     if distance_to_center < self.radius:
+        #         self.ray_values[i] = 1
+        #     else:
+        #         self.ray_values[i] = -1
+
+        # Using numpy arrays for faster computation
+        ray_x = self.car_x + self.ray_lengths * np.cos(self.car_angle + self.ray_angles)
+        ray_y = self.car_y - self.ray_lengths * np.sin(self.car_angle + self.ray_angles)
+
+        # Calculate distance_to_center for all rays at once
+        distance_to_center = np.sqrt((ray_x - self.center[0]) ** 2 + (ray_y - self.center[1]) ** 2)
+
+        # Update ray_values based on whether the rays are inside or outside the circular path of thickness path_thickness
+        self.observation[:-3] = np.where(((distance_to_center < self.radius) & (distance_to_center > self.inner_radius)), 1, -1)
+
 
         '''
         # Update score based on radar ray values
@@ -83,32 +100,41 @@ class ContinuousCarRadarEnv:
         
         # Check if the car is inside the circular path
         distance_to_center = math.sqrt((self.car_x - self.center[0]) ** 2 + (self.car_y - self.center[1]) ** 2)
-        if self.radius - 50 < distance_to_center < self.radius:
+        if self.inner_radius < distance_to_center < self.radius:
             reward = self.reward_inside_path
         else:
             reward = self.reward_outside_path
-        self.score = reward
 
         # Check if the car has reached the goal
         done = False
         if reward == self.reward_inside_path:
-            if np.allclose(self.car_x, self.center[0]) and np.allclose(self.car_y, self.center[1]):
+            # If car has crossed the finish line, done = True
+            if self.half_lap_done == True and self.car_y < self.center[1] and self.car_x < self.center[0] + self.radius and self.car_x > self.center[0] +  self.inner_radius:
                 done = True
+                reward = 100
 
-        # Return observation, reward, done, info
-        observation = np.array(self.ray_values + [self.car_x, self.car_y, self.car_angle])
-        return observation, reward, done, {}
+        # Check if the car has crossed half the lap
+        if self.half_lap_done == False and self.car_y > self.center[1] and self.car_x > self.center[0] - self.radius and self.car_x < self.center[0] -  self.inner_radius: 
+            self.half_lap_done = True
+        
+        # Update observation and score
+        self.score = reward
+        self.observation[-3:] = [self.car_x, self.car_y, self.car_angle]
+        return self.observation, reward, done, {}
 
     def reset(self):
         # Reset car position and radar values
-        self.car_x, self.car_y = self.center[0] + self.radius - 25, self.center[1]
+        self.car_x, self.car_y = self.center[0] + self.radius - self.path_thickness/2, self.center[1]
         self.car_angle = math.pi / 2
-        self.ray_values = [0] * self.num_rays
+        # self.ray_values = [0] * self.num_rays
         self.score = 0
+        self.half_lap_done = False
+        self.done = False
 
-        # Return initial observation
-        observation = np.array(self.ray_values + [self.car_x, self.car_y, self.car_angle])
-        return observation
+        # Reset observation
+        self.observation[:-3] = 0
+        self.observation[-3:] = [self.car_x, self.car_y, self.car_angle]
+        return self.observation
 
     def render(self):
         # Main rendering loop
@@ -116,7 +142,7 @@ class ContinuousCarRadarEnv:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
-        self.clock.tick(60)
+        # self.clock.tick(120)
 
         # Clear the screen
         self.screen.fill(self.white)
@@ -142,8 +168,8 @@ class ContinuousCarRadarEnv:
         # Draw score box
         score_box = pygame.Surface((self.window_size[0], 100))
         score_box.fill((200, 200, 200))
-        font = pygame.font.Font(None, 24)
-        text = font.render(f"State: {self.ray_values + [self.car_x, self.car_y, self.car_angle]}  Score: {self.score}", True, (0, 0, 0))
+        font = pygame.font.Font(None, 22)
+        text = font.render(f"State: {self.observation}  Score: {self.score}", True, (0, 0, 0))
         score_box.blit(text, (10, 10))
         self.screen.blit(score_box, (0, self.window_size[1] - 100))
         
@@ -160,13 +186,17 @@ env = ContinuousCarRadarEnv()
 # Example of using the environment
 state = env.reset()
 env.render()
-for i in range(1350):
-    if i%17 == 0:
+for i in range(1850):
+    if i%26 == 0:
         action = 0
     else:
         action = 1
+    # action=1
     next_state, reward, done, _ = env.step(action)
     env.render()
+    if done:
+        print("Done")
+        break
 
 # Quit Pygame
 pygame.quit()
